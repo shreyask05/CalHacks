970a9c27d31d52cbd38b3bb37035aaf3fa809976
rohanrrb <rohanbopi@gmail.com>
2024-10-18 20:19:41 -0700

    navbar

--- CalHacks/CalHacks.py
+++ CalHacks/CalHacks.py
@@ -39,10 +39,76 @@ def index() -> rx.Component:
         rx.logo(),
     )
 
+def navbar_link(text: str, url: str) -> rx.Component:
+    return rx.link(
+        rx.text(text, size="4", weight="medium"), href=url
+    )
+
+
+def navbar() -> rx.Component:
+    return rx.box(
+        rx.desktop_only(
+            rx.hstack(
+                rx.hstack(
+                    rx.heading(
+                        "Swarm", size="7", weight="bold"
+                    ),
+                    align_items="left",
+                ),
+                rx.hstack(
+                    navbar_link("Home", "/#"),
+                    justify="end",
+                    spacing="5",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        rx.mobile_and_tablet(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="6", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.menu.root(
+                    rx.menu.trigger(
+                        rx.icon("menu", size=30)
+                    ),
+                    rx.menu.content(
+                        rx.menu.item("Home"),
+                    ),
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        bg=rx.color("accent", 3),
+        padding="1em",
+        # position="fixed",
+        # top="0px",
+        # z_index="5",
+        width="100%",
+    )
 def about():
-    return rx.text("for calhacks 24'")
+    return rx.box(
+        navbar(),
+        rx.text("for calhacks 24'")
+        )
 def dashboard():
-    return rx.text("dashboard here")
+
+    return rx.box(
+        navbar(),
+        rx.text("the dashboard lives here")
+        )
 
 app = rx.App()
 app.add_page(index)
