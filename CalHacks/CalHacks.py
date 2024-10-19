d601496ea6e846aecb9cf60396d514dd7c96bd5a
rohanrrb <rohanbopi@gmail.com>
2024-10-18 19:35:43 -0700

    test commit

--- CalHacks/CalHacks.py
+++ CalHacks/CalHacks.py
@@ -18,7 +18,7 @@ def index() -> rx.Component:
         rx.vstack(
             rx.heading("Welcome to Reflex!", size="9"),
             rx.text(
-                "Get started by editing ",
+                "Code Tester!!!! ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
