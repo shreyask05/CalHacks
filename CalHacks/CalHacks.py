2711c429edadf047684be58c057a7cad8a9a4998
rohanrrb <rohanbopi@gmail.com>
2024-10-18 21:39:10 -0700

    i forgot im sorry

--- CalHacks/CalHacks.py
+++ CalHacks/CalHacks.py
@@ -98,6 +98,43 @@ def navbar() -> rx.Component:
         # z_index="5",
         width="100%",
     )
+
+class FormState(rx.State):
+    form_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        """Handle the form submit."""
+        self.form_data = form_data
+
+
+def form():
+    return rx.vstack(
+        rx.form(
+            rx.vstack(
+                rx.input(
+                    placeholder="First Name",
+                    name="first_name",
+                ),
+                rx.input(
+                    placeholder="Last Name",
+                    name="last_name",
+                ),
+                rx.hstack(
+                    rx.checkbox("Checked", name="check"),
+                    rx.switch("Switched", name="switch"),
+                ),
+                rx.button("Submit", type="submit"),
+            ),
+            on_submit=FormState.handle_submit,
+            reset_on_submit=True,
+        ),
+        rx.divider(),
+        rx.heading("Results"),
+        rx.text(FormState.form_data.to_string()),
+    )
+
+
+
 def about():
     return rx.box(
         navbar(),
@@ -107,6 +144,8 @@ def dashboard():
 
     return rx.box(
         navbar(),
+        rx.divider(),
+        form(),
         rx.text("the dashboard lives here")
         )
 
@@ -114,3 +153,4 @@ app = rx.App()
 app.add_page(index)
 app.add_page(dashboard)
 app.add_page(about)
+
